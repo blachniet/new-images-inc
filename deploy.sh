e8cbcdfc24b73eaf8fed5d3e04ccee99d1509fbd
Brian Lachniet <blachniet@gmail.com>
2021-01-24 14:23:56 -0500

    deploy.sh: Save the CNAME file

--- deploy.sh
+++ deploy.sh
@@ -17,11 +17,17 @@ rm -rf .git/worktrees/public/
 echo "Checking out gh-pages branch into public"
 git worktree add -B gh-pages public origin/gh-pages
 
+echo "Setting aside the CNAME file"
+mv public/CNAME .
+
 echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
 hugo
+
+echo "Putting the CNAME file back"
+mv CNAME public/
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Deploy $(git rev-parse --short HEAD) to gh-pages (deploy.sh)"
