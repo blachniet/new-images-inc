ff61a80325655919bd761cef904170e374913a3f
Brian Lachniet <blachniet@gmail.com>
2021-03-14 12:08:20 -0400

    Add sidenav and transparent logos

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Business card
+        'niblue-dark': 'rgb(0, 105, 200)', // dark color at top of water
+        'niblue-light': 'rgb(0, 168, 230)', // light color at bottom of water
+        niblue: 'rgb(0, 131, 204)', // blue square
+        niorange: 'rgb(255, 120, 0)', // orange square
+        nidark: 'rgb(32, 32, 32)', // background color
+      }
+    },
   },
   variants: {
     extend: {},
